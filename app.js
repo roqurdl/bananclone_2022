d4dde1f465760daf8164a8097892ddf978f98377
roqurdl <uyhnri@gmail.com>
2022-05-13 22:22:10 +0900

    banana_220513

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username);
+  localStorage.setItem(USERNAME_KEY, username);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function linkClick(event) {
-  event.preventDefault();
-  alert("clicked!");
-  console.log(event);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", linkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  greeting.innerText = `Hello ${savedUsername}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}

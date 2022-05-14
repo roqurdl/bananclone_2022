5cb1298fb268c524d74638823d1f00069e916652
roqurdl <uyhnri@gmail.com>
2022-05-14 21:10:20 +0900

    banana.220514

--- js/greetings.js
+++ js/greetings.js
@@ -10,8 +10,7 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
 function paintGreetings(username) {
@@ -25,6 +24,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  greeting.innerText = `Hello ${savedUsername}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(savedUsername);
 }
---
+++ js/todo.js
@@ -0,0 +1,36 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDO(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDO);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit() {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  toDos.push(newTodo);
+  paintToDo(newTodo);
+  saveToDos();
+}
+toDoForm.addEventListener("submit", handleToDoSubmit);
